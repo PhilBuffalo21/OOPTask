4d9808bd23ddc0eab561a8889b73e66c97522b6a
KrisTheMuggle <kris.themuggle@gmail.com>
2023-11-13 18:35:37 +1100

    <Update> Payment Processor 1.1

--- Payment/PaymentProcessor.java
+++ Payment/PaymentProcessor.java
@@ -3,7 +3,7 @@ package OOPTasks.Payment;
 import java.util.List;
 import java.util.function.Consumer;
 
-public abstract class PaymentProcessor<T extends Payment> {
+public class PaymentProcessor<T extends Payment> {
     public void processor(List<T> payments, Consumer<T> processingStrategy) {
         payments.forEach(processingStrategy);
     }
