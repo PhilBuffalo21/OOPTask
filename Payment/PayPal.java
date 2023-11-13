4744531e7b2f0f7a2b083148a057edebeb4a81ec
KrisTheMuggle <kris.themuggle@gmail.com>
2023-11-13 18:19:32 +1100

    <Completed> PayPal Payment Process

--- Payment/PayPal.java
+++ Payment/PayPal.java
@@ -1,11 +1,20 @@
 package OOPTasks.Payment;
 
 public class PayPal implements Payment {
+    Integer value;
+
+    public PayPal(Integer n) {
+        value = n;
+    }
+
+    public PayPal() {
+        value = 0;
+    }
 
     @Override
     public void processPayment() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'processPayment'");
+        // Fees
+        value += 2;
     }
 
 }
